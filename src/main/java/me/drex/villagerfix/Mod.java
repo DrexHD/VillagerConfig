eda37a5c698a790740a74b52dd6c8789ba2299f2
DrexHD <nicknamedrex@gmail.com>
2020-08-11 00:05:57 +0200

    Fix client compability

--- src/main/java/me/drex/villagerfix/Mod.java
+++ src/main/java/me/drex/villagerfix/Mod.java
@@ -1,13 +1,20 @@
 package me.drex.villagerfix;
 
 import me.drex.villagerfix.commands.Main;
-import net.fabricmc.api.DedicatedServerModInitializer;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-public class Mod implements DedicatedServerModInitializer {
+
+public class Mod implements ModInitializer {
+
+    public static final Logger LOGGER = LogManager.getFormatterLogger("VillagerFix");
 
     @Override
-    public void onInitializeServer() {
+    public void onInitialize() {
+        LOGGER.info("Initalizing VillagerFix!");
         CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {
             new Main().register(dispatcher);
         });
