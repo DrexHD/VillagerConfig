3e09b34df959ebd450993b1025e8c294bf03e84d
DrexHD <nicknamedrex@gmail.com>
2020-11-04 19:16:54 +0100

    fix crash on server

--- src/main/java/me/drex/villagerfix/entry/AbstractMod.java
+++ src/main/java/me/drex/villagerfix/entry/AbstractMod.java
@@ -1,29 +1,28 @@
-package me.drex.villagerfix;
+package me.drex.villagerfix.entry;
 
+import me.drex.villagerfix.VillagerFix;
 import me.drex.villagerfix.commands.Commands;
 import me.drex.villagerfix.config.Config;
-import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 
-public class Mod implements ModInitializer {
+public class AbstractMod {
 
     public static final Logger LOGGER = LogManager.getFormatterLogger("VillagerFix");
 
-    @Override
-    public void onInitialize() {
+    public void load() {
         LOGGER.info("Initalizing VillagerFix!");
         CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {
             new Commands().register(dispatcher);
         });
         new VillagerFix();
-        ClientLifecycleEvents.CLIENT_STARTED.register(client -> {
-            if (!Config.isConfigLoaded) {
-                Config.load();
-            }
-        });
+    }
+
+    public void loadConfig() {
+        if (!Config.isConfigLoaded) {
+            Config.load();
+        }
     }
 }
