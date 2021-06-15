aa75b46dc0e2a6776b11ae3e6ed3e90251deae6d
DrexHD <nicknamedrex@gmail.com>
2021-06-15 09:57:38 +0200

    fix: Mixin method parameters

--- src/main/java/me/drex/villagerfix/mixin/TradeOutputSlotMixin.java
+++ src/main/java/me/drex/villagerfix/mixin/TradeOutputSlotMixin.java
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TradeOutputSlot.class)
 public class TradeOutputSlotMixin {
@@ -21,7 +21,7 @@ public class TradeOutputSlotMixin {
     @Shadow @Final private Merchant merchant;
 
     @Inject(method = "onTakeItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/village/Merchant;trade(Lnet/minecraft/village/TradeOffer;)V"))
-    public void onTrade(PlayerEntity player, ItemStack stack, CallbackInfoReturnable<ItemStack> cir) {
+    public void onTrade(PlayerEntity player, ItemStack stack, CallbackInfo ci) {
         if (ConfigEntries.oldTrades.enabled) {
             if (Helper.chance(ConfigEntries.oldTrades.unlockChance)) {
                 this.merchant.getOffers().forEach(tradeOffer -> {
