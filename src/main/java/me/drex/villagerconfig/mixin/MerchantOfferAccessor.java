5780928cee3e669889e69e826de93fea6f4d1e7c
Drex <nicknamedrex@gmail.com>
2024-02-01 21:41:12 +0100

    Fix crash on 24w05b

--- src/main/java/me/drex/villagerconfig/mixin/MerchantOfferAccessor.java
+++ src/main/java/me/drex/villagerconfig/mixin/MerchantOfferAccessor.java
@@ -2,12 +2,14 @@ package me.drex.villagerconfig.mixin;
 
 import net.minecraft.world.item.trading.MerchantOffer;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(MerchantOffer.class)
 public interface MerchantOfferAccessor {
 
     @Accessor
+    @Mutable
     void setRewardExp(boolean rewardExp);
 
 }
