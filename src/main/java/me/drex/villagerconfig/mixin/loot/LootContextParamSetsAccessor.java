103bca6d8712a41be2639c6b5a61923aa13e9950
Drex <nicknamedrex@gmail.com>
2024-05-24 15:52:04 +0200

    Fix crash when generating trades

--- src/main/java/me/drex/villagerconfig/mixin/loot/LootContextParamSetsAccessor.java
+++ src/main/java/me/drex/villagerconfig/mixin/loot/LootContextParamSetsAccessor.java
@@ -1,18 +1,16 @@
 package me.drex.villagerconfig.mixin.loot;
 
-import com.google.common.collect.BiMap;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSet;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+import java.util.function.Consumer;
 
 @Mixin(LootContextParamSets.class)
 public interface LootContextParamSetsAccessor {
-
-    @Accessor
-    static BiMap<ResourceLocation, LootContextParamSet> getREGISTRY() {
+    @Invoker
+    static LootContextParamSet invokeRegister(String name, Consumer<LootContextParamSet.Builder> type) {
         throw new AssertionError();
     }
-
 }
--- src/main/java/me/drex/villagerconfig/util/loot/VCLootContextParams.java
+++ src/main/java/me/drex/villagerconfig/util/loot/VCLootContextParams.java
@@ -7,14 +7,12 @@ import net.minecraft.world.level.storage.loot.parameters.LootContextParamSet;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
 
 import java.util.Map;
-import java.util.function.Consumer;
 
 import static me.drex.villagerconfig.VillagerConfig.MOD_ID;
 
 public class VCLootContextParams {
-
     public static final LootContextParam<Map<String, Float>> NUMBER_REFERENCE = create("number_reference");
-    public static final LootContextParamSet VILLAGER_LOOT_CONTEXT = register("villager", builder -> new LootContextParamSet.Builder().required(LootContextParams.ORIGIN).required(LootContextParams.THIS_ENTITY).required(NUMBER_REFERENCE));
+    public static final LootContextParamSet VILLAGER_LOOT_CONTEXT = LootContextParamSetsAccessor.invokeRegister("villager", builder -> builder.required(LootContextParams.ORIGIN).required(LootContextParams.THIS_ENTITY).required(NUMBER_REFERENCE));
 
     public static void init() {
     }
@@ -22,19 +20,4 @@ public class VCLootContextParams {
     private static <T> LootContextParam<T> create(String string) {
         return new LootContextParam<>(ResourceLocation.fromNamespaceAndPath(MOD_ID, string));
     }
-
-    private static LootContextParamSet register(String string, Consumer<LootContextParamSet.Builder> consumer) {
-        // [VanillaCopy] - LootContextParamSets.register
-        LootContextParamSet.Builder builder = new LootContextParamSet.Builder();
-        consumer.accept(builder);
-        LootContextParamSet lootContextParamSet = builder.build();
-        ResourceLocation resourceLocation = ResourceLocation.fromNamespaceAndPath(MOD_ID, string); // ResourceLocation.withDefaultNamespace -> ResourceLocation.fromNamespaceAndPath
-        LootContextParamSet lootContextParamSet2 = LootContextParamSetsAccessor.getREGISTRY().put(resourceLocation, lootContextParamSet);
-        if (lootContextParamSet2 != null) {
-            throw new IllegalStateException("Loot table parameter set " + resourceLocation + " is already registered");
-        } else {
-            return lootContextParamSet;
-        }
-    }
-
 }
