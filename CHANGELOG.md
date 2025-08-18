ccc67b7ca282b5d7079bbc5b11363051431bb406
Drex <nicknamedrex@gmail.com>
2025-08-18 14:25:10 +0200

    Fix log error when playing without cloth-config

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,8 +6,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [4.4.7] - 2025-08-18
 ### Fixed
 - Config path location. The config was loaded from `config/villagerconfig.json5` from v4.4.0 to v4.4.6, but should have been loaded from `config/VillagerConfig/villagerconfig.json5`. **Make sure to migrate any changes you made in the wrong file to the fixed location.**
+- Log error when playing without `cloth-config`
 
 ## [4.4.6] - 2025-08-05
 ### Fixed
--- fabric/src/main/java/me/drex/villagerconfig/fabric/config/modmenu/VillagerConfigModMenuEntry.java
+++ fabric/src/main/java/me/drex/villagerconfig/fabric/config/modmenu/VillagerConfigModMenuEntry.java
@@ -3,12 +3,17 @@ package me.drex.villagerconfig.fabric.config.modmenu;
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
 import me.drex.villagerconfig.common.config.ConfigScreen;
+import me.drex.villagerconfig.common.platform.PlatformHooks;
 
 public class VillagerConfigModMenuEntry implements ModMenuApi {
 
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return ConfigScreen::getConfigScreen;
+        if (PlatformHooks.PLATFORM_HELPER.isModLoaded("cloth-config")) {
+            return ConfigScreen::getConfigScreen;
+        } else {
+            return ModMenuApi.super.getModConfigScreenFactory();
+        }
     }
 
 }
